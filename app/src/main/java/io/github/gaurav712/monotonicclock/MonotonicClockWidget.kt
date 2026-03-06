30bebd48ffe9509ddf3ac82d59702fb3bcae2834
Gaurav Kumar Yadav <gaurav712@protonmail.com>
2026-03-07 00:23:18 +0530

    add pending changes; upgrade gradle

--- app/src/main/java/io/github/gaurav712/monotonicclock/MonotonicClockWidget.kt
+++ app/src/main/java/io/github/gaurav712/monotonicclock/MonotonicClockWidget.kt
@@ -1,16 +1,63 @@
 package io.github.gaurav712.monotonicclock
 
+import android.annotation.SuppressLint
 import android.content.Context
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
 import androidx.glance.GlanceId
+import androidx.glance.GlanceModifier
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.provideContent
+import androidx.glance.layout.Alignment
+import androidx.glance.layout.Column
+import androidx.glance.layout.fillMaxSize
+import androidx.glance.text.FontFamily
 import androidx.glance.text.Text
+import androidx.glance.text.TextStyle
+import androidx.glance.unit.ColorProvider
+import kotlinx.coroutines.delay
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
 
 class MonotonicClockWidget : GlanceAppWidget() {
 
     override suspend fun provideGlance(context: Context, id: GlanceId) {
         provideContent {
-            Text("Hello World")
+            MonotonicClock()
         }
+    }
+}
+
+@SuppressLint("RestrictedApi")
+@Composable
+fun MonotonicClock() {
+    var currentTime by remember { mutableStateOf(LocalTime.now()) }
+
+    LaunchedEffect(Unit) {
+        while (true) {
+            currentTime = LocalTime.now()
+            delay(1000)
+        }
+    }
+
+    Column(
+        modifier = GlanceModifier.fillMaxSize(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = currentTime.format(DateTimeFormatter.ofPattern("HH:mm")),
+            style = TextStyle(
+                color = ColorProvider(Color.White),
+                fontSize = 80.sp,
+                fontFamily = FontFamily("@font/share_tech_mono_regular")
+            )
+        )
     }
 }
